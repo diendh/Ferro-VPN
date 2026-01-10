eded53c9097ba627ff9718e3ea285f239f294b89
Tom <>
2026-01-10 11:33:03 +0700

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ferro.vietts.app',
+  build: {
+    assets: 'assets'
+  },
   vite: {
     plugins: [tailwindcss()]
   },
