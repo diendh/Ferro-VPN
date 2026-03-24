85b2449f1f7735b1a8565ed43877e36782ccf134
Tom <diendh@vietts.io>
2026-03-24 15:28:48 +0700

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ferro.vietts.app',
+  adapter: cloudflare(),
   build: {
     assets: 'assets'
   },
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [alpinejs()]
 });
